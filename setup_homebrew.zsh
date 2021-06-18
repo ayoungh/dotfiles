6c4214815b09d621e51ee303b8eaae3e9c0504e5
Anthony Young <404328+ayoungh@users.noreply.github.com>
2021-06-18 11:35:07 +0100

    Add homebrew install to dotbot config

---
+++ setup_homebrew.zsh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting Homebrew Setup >>>\n"
+
+if exists brew; then
+  echo "brew exists, skipping install"
+else
+  echo "brew doesn't exist, continuing with install"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+brew install httpie
+brew install bat
+
+brew install google-chrome
